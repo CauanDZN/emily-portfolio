7ee4374590ca045580052cacc8c713a7b3d5df69
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-04 22:17:06 -0300

    feat: adding features

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Header } from './components/Header'
+import { Page } from './components/Pages/Home';
+import { GlobalStyle } from './global'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ nav, setNav ] = useState('home');
+  const [colorScheme, setColorScheme] = useState('dark');
+
+  function handleChangeNav(name:string){
+    setNav(name);
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <Header changeNav={handleChangeNav} nav={nav} />
+      <Page changeNav={handleChangeNav}/>
+      <GlobalStyle />
+    </>
   )
 }
 
---
+++ src/components/Pages/Home/data.ts
@@ -0,0 +1,112 @@
+import React from 'react';
+
+import anos60_1 from '../../../assets/images/anos60_1.jpeg';
+import anos60_2 from '../../../assets/images/anos60_2.jpeg';
+import anos60_3 from '../../../assets/images/anos60_3.jpeg';
+import anos60_4 from '../../../assets/images/anos60_4.jpeg';
+
+
+export const letSummary1 = [
+  {
+    infoTitle: 'Fotografia',
+    value: '91%'
+  },
+
+  {
+    infoTitle: 'Comunicação',
+    value: '90%'
+  },
+
+  {
+    infoTitle: 'Proatividade',
+    value: '90%'
+  },
+];
+
+export const letSummary2 = [
+  {
+    infoTitle: 'Midias Sociais',
+    value: '85%'
+  },
+
+  {
+    infoTitle: 'Marketing Digital',
+    value: '75%'
+  },
+
+  {
+    infoTitle: 'Edição de fotos',
+    value: '92%'
+  },
+];
+
+export const letContact = [
+  {
+    text: 'Instagram',
+    description: '@emy.leey',
+    image: 'Instagram',
+    link: 'https://www.instagram.com/emy.leey/',
+    type: 'contact'
+  },
+  {
+    text: 'WhatsApp',
+    description: '(85) 9 9683-4563',
+    image: 'Whatsapp',
+    link: 'https://wa.me/+5585996834563',
+    type: 'contact'
+  },
+]
+
+export const Projetos = [{
+  titulo: "Anos 60",
+  key: 9890,
+  descricao: "Trabalhei em um projeto de exposição e confecções de roupas de décadas",
+  items: [{
+      src: anos60_1,
+      alt: "Garota com roupa bem produzida",
+      titulo: "Sobre",
+      texto: "Poses e looks inspirados na vivência da sociedade nos anos 60",
+      key: 3412411
+  },]
+}, {
+  titulo: "Anos 60 - Fotografia com Modelo",
+  key: 44132,
+  descricao: 'Editorial fotográfico com o tema "Exposição e Confecções de roupas de Décadas"',
+  items: [{
+      src: anos60_2,
+      alt: "Garota com roupa bem produzida",
+      titulo: "Sobre",
+      texto: "Poses e looks inspirados na vivência da sociedade",
+      key: 53412
+  }, {
+      src: anos60_3,
+      alt: "Garota com roupa bem produzida",
+      titulo: "Sobre",
+      texto: "Poses e looks inspirados na vivência da sociedade",
+      key: 64532
+  }, {
+      src: anos60_4,
+      alt: "Garota com roupa bem produzida",
+      titulo: "Sobre",
+      texto: "Poses e looks inspirados na vivência da sociedade",
+      key: 7562341
+  }]
+}, ];
+
+export const letCourses = [
+  {
+    text: 'Como falar em público',
+    description: 'Google',
+    image: 'Cert',
+    link: null,
+    type: 'cert'
+  },
+
+  {
+    text: 'Como organizar eventos de sucesso',
+    description: 'Doity',
+    image: 'Cert',
+    link: null,
+    type: 'cert'
+  },
+]
